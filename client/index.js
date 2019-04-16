27f174d5e2bea8b3a2e4b058160b2b172e782a3c
BenRamsden <benramsdensii@gmail.com>
2019-04-16 21:54:56 +0100

    Create a user class for testing with multiple users

--- client/index.js
+++ client/index.js
@@ -3,28 +3,46 @@ const {AUTHENTICATE, AUTHENTICATE_SUCCESS, CHECK_CONNECTIONS} = require("../shar
 const WebSocket = require('ws');
 const { toEvent } = require('../shared-helpers/event')
 
-console.log('opening websocket connection');
-const s = new WebSocket('ws://localhost:3000/');
-s.addEventListener('error', (m) => {
-    console.log("error");
-});
-
-s.addEventListener('open', (m) => {
-    console.log("websocket connection open");
-    s.send(JSON.stringify({
-        type:AUTHENTICATE,
-        payload: 'userid123'
-    }))
-});
-
-s.addEventListener('message', (m) => {
-    const event = toEvent(m.data)
-    console.log(event)
-
-    if(event.type===AUTHENTICATE_SUCCESS) {
-        s.send(JSON.stringify({
-            type:CHECK_CONNECTIONS,
-            payload:null
-        }))
+class User {
+    constructor(userId) {
+        this.userId = userId
+        this.connections = []
     }
-});
+
+    connect() {
+        const s = new WebSocket('ws://localhost:3000/');
+        s.addEventListener('error', (m) => {
+            console.log("error")
+        })
+        s.addEventListener('open',m => {
+            s.send(JSON.stringify({
+                type:AUTHENTICATE,
+                payload: this.userId
+            }))
+        })
+        s.addEventListener('message', (m) => {
+            const event = toEvent(m.data)
+            console.log(event)
+
+            if(event.type===AUTHENTICATE_SUCCESS) {
+                s.send(JSON.stringify({
+                    type:CHECK_CONNECTIONS,
+                    payload:null
+                }))
+            }
+        })
+        this.connections.push(s)
+    }
+
+    disconnect() {
+        if(this.connections.length===0) {
+            throw new Error("Cannot disconnect, no connections")
+        }
+
+        this.connections[this.connections.length-1].close()
+    }
+}
+
+const user = new User('user123')
+
+user.connect()
