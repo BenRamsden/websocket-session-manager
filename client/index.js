b0adf81c3982577029cd1b09f7d1d98f5b1ee08e
BenRamsden <benramsdensii@gmail.com>
2019-04-16 21:32:49 +0100

    Configure simple messaging system

--- client/index.js
+++ client/index.js
@@ -8,8 +8,29 @@ s.addEventListener('error', (m) => {
 
 s.addEventListener('open', (m) => {
     console.log("websocket connection open");
+    s.send(JSON.stringify({
+        type:'authenticate',
+        payload: 'userid123'
+    }))
 });
 
+const toEvent = (message) => {
+    const event = JSON.parse(message)
+
+    if(typeof event.type !== 'string') throw new Error()
+    if(typeof event.payload !== 'object' && typeof event.payload !== 'string') throw new Error()
+
+    return { type: event.type, payload: event.payload }
+}
+
 s.addEventListener('message', (m) => {
-    console.log(m.data);
+    const event = toEvent(m.data)
+    console.log(event)
+
+    if(event.type==='authenticate_success') {
+        s.send(JSON.stringify({
+            type:'check_connections',
+            payload:null
+        }))
+    }
 });
--- server/index.js
+++ server/index.js
@@ -7,9 +7,34 @@ app.get('/', (req, res) => {
     res.send({message:"Web socket is needed to talk to this server"});
 });
 
+const toEvent = (message) => {
+    const event = JSON.parse(message)
+
+    if(typeof event.type !== 'string') throw new Error()
+    if(typeof event.payload !== 'object' && typeof event.payload !== 'string') throw new Error()
+
+    return { type: event.type, payload: event.payload }
+}
+
 app.ws('/', (s, req) => {
     console.log('websocket connection');
-    s.send('message from server')
+    s.send(JSON.stringify({
+        type:'hello',
+        payload: 'message from server'
+    }))
+    s.on('message',message => {
+        const event = toEvent(message)
+        console.log(event.type,"event received from user",s.userId)
+
+        if(event.type==='authenticate') {
+            const userId = event.payload
+            s.userId = userId
+            s.send(JSON.stringify({
+                type:'authenticate_success',
+                payload: 'user authenticated'
+            }))
+        }
+    })
 });
 
 app.listen(3000, () => console.log('listening on http://localhost:3000/'));
